10014b003e0c699dfe4b57ec710df11466293d14
fugui-java <fugui52361>
2020-04-26 21:50:14 +0800

    chat 工程

---
+++ lzs-chat-service/src/main/java/com/lzs/chat/service/service/RoomInfoService.java
@@ -0,0 +1,13 @@
+package com.lzs.chat.service.service;
+
+/**
+ * <一句话说明功能>
+ * <功能详细描述>
+ *
+ * @author fugui
+ * @title <一句话说明功能>
+ * @date 2020/4/26 21:49
+ * @since <版本号>
+ */
+public interface RoomInfoService {
+}
